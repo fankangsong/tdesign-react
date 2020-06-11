ca06e2af5427359f3c5d90abebce51b73d1bd5ba
Yacent <540608491@qq.com>
2020-06-11 19:42:39 +0800

    feat: add baseTable component

---
+++ src/table/README.md
@@ -0,0 +1,9 @@
+# Table 表格
+
+提供标准的表格交互和样式
+
+## 使用示例
+
+### 基本用法
+
+[Example: 表格示例](./_example/TableExample.jsx)
---
+++ src/table/_example/TableExample.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Table from '../base/Table';
+
+const exampleList = [
+  {
+    projectName: 'TDesign',
+    manager: ['yacentlin', 'grayqin', 'sheepluo', 'cache'],
+  },
+];
+
+export default function TableExample() {
+  return (
+    <Table
+      records={exampleList}
+      columns={[
+        {
+          key: 'project',
+          title: '项目名称',
+          width: '15%',
+          render: (x) => x.projectName,
+        },
+        {
+          key: 'memeber',
+          title: '管理员',
+          width: '50%',
+          render: (x) => x.manager.join(','),
+        },
+      ]}
+      rowKey="projectName"
+    />
+  );
+}
