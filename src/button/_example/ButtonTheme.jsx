beddf73051d038e1b95feee3c0e4a4305c5bc18f
xughv <vectorxu@qq.com>
2020-07-16 21:29:13 +0800

    refactor: button structure update

--- src/button/_example/ButtonTheme.jsx
+++ src/button/_example/ButtonTheme.jsx
@@ -10,10 +10,8 @@ export default function ButtonExample() {
       <Button theme="warning">warning</Button>
       <Button theme="warning-line">warning-line</Button>
       <Button theme="link">link</Button>
-      <div style={{ background: '#ddd', padding: '0 8px 8px 8px', marginTop: 8 }}>
-        <Button theme="ghost">ghost</Button>
-        <Button theme="ghost-line">ghost-line</Button>
-      </div>
+      <Button theme="ghost">ghost</Button>
+      <Button theme="ghost-line">ghost-line</Button>
     </>
   );
 }
