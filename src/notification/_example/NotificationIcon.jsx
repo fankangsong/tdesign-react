21c7a0206d9c299e4989869b72a802b3b405af0c
yorickili <yorickili@qq.com>
2020-07-18 15:41:22 +0800

    feat: add notification component

---
+++ src/notification/_example/NotificationIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Notification, IconFont } from '@tdesign/react';
+
+export default function NotificationExample() {
+  return (
+    <Notification
+      title="自定义icon"
+      content="这是一条消息通知"
+      icon={<IconFont name="more" />}
+    />
+  );
+}
