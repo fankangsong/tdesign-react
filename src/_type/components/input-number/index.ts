ee9f8ce62a0432afaf7371e958d9837cd9abeb87
chaishi <974383157@qq.com>
2021-03-23 11:17:02 +0800

    docs(input-number): add decimalPlaces api

--- src/_type/components/input-number/index.ts
+++ src/_type/components/input-number/index.ts
@@ -1,11 +1,16 @@
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
- * updated at 2021-03-22 22:15:40
+ * updated at 2021-03-23 11:15:31
  * */
 
 import { MouseEvent, KeyboardEvent, FocusEvent, FormEvent } from 'react';
 
 export interface TdInputNumberProps {
+  /**
+   * [小数位数](https://en.wiktionary.org/wiki/decimal_place)
+   * @default 0
+   */
+  decimalPlaces?: number;
   /**
    * 禁用组件
    * @default false
