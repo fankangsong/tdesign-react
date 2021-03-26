8f76a6bf5d1035bf6044307541d5927983977ca5
chaishi <974383157@qq.com>
2021-03-26 17:12:27 +0800

    docs(checkbox): update ChehckboxOptionObj

--- src/_type/components/checkbox/index.ts
+++ src/_type/components/checkbox/index.ts
@@ -1,6 +1,6 @@
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
- * updated at 2021-03-26 12:06:47
+ * updated at 2021-03-26 17:11:30
  * */
 
 import { TNode } from '../../common';
@@ -31,6 +31,11 @@ export interface TdCheckboxProps {
    */
   name?: string;
   /**
+   * 组件是否只读
+   * @default false
+   */
+  readonly?: boolean;
+  /**
    * 复选框的值
    */
   value?: string | number;
@@ -52,7 +57,7 @@ export interface TdCheckboxGroupProps {
    */
   name?: string;
   /**
-   * 以配置形式设置子元素。当 CheckboxOption 类型为 string 或 number 时，表示 label 和 value 值相同。type 值为 check-all 表示当前选项为「全选选项」
+   * 以配置形式设置子元素。当 CheckboxOption 类型为 string 或 number 时，表示 label 和 value 值相同。checkAll 值为 true 表示当前选项为「全选选项」
    * @default []
    */
   options?: Array<CheckboxOption>;
@@ -74,6 +79,6 @@ export interface TdCheckboxGroupProps {
 
 export type CheckboxOption = string | number | CheckboxOptionObj;
 
-export interface CheckboxOptionObj { label?: string | TNode; value?: string | number; disabled?: boolean; name?: string; type?: 'check-all' };
+export interface CheckboxOptionObj { label?: string | TNode; value?: string | number; disabled?: boolean; name?: string; checkAll?: true };
 
 export type CheckboxGroupValue = Array<string | number> | string | number;
