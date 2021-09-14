c059e54926671d192b352f02b364e09bc8832955
dreamsqin <419866272@qq.com>
2021-09-14 14:35:53 +0800

    feat: init comment folder

---
+++ src/_type/components/comment/index.ts
@@ -0,0 +1,41 @@
+/* eslint-disable */
+
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * updated at 2021-09-14 11:26:08
+ * */
+
+import { AvatarProps } from '../../../avatar';
+import { TNode, TElement } from '../../common';
+
+export interface TdCommentProps {
+  /**
+   * 操作
+   */
+  actions?: Array<TNode>;
+  /**
+   * 作者
+   */
+  author?: TNode;
+  /**
+   * 头像
+   */
+  avatar?: AvatarProps | TNode;
+  /**
+   * 内容
+   */
+  content?: TNode;
+  /**
+   * 时间
+   */
+  datetime?: TNode;
+  /**
+   * 回复列表
+   */
+  reply?: TElement;
+  /**
+   * 评论风格，共两种：主评论 和 引用
+   * @default comment
+   */
+  theme?: 'comment' | 'quote';
+}
