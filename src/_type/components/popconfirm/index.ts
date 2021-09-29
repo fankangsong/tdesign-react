fc62dee4df369d8cfee2090bd2c9ed129e9924d9
HQ-Lin <hq.lin@foxmail.com>
2021-09-29 15:25:22 +0800

    feat: 同步 api

--- src/_type/components/popconfirm/index.ts
+++ src/_type/components/popconfirm/index.ts
@@ -1,17 +1,19 @@
+/* eslint-disable */
+
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
- * updated at 2021-06-02 17:34:44
+ * updated at 2021-09-29 15:22:46
  * */
 
-import { MouseEvent } from 'react';
 import { ButtonProps } from '../../../button';
 import { PopupProps } from '../../../popup';
 import { TNode, TElement } from '../../common';
+import { MouseEvent } from 'react';
 
 export interface TdPopconfirmProps {
   /**
-   * 取消按钮
-   * @default '取消'
+   * 取消按钮，可自定义。值为 undefined 或 null 则不显示取消按钮。值类型为 Object 则表示透传 Button 组件属性
+   * @default ''
    */
   cancelBtn?: ButtonProps | TNode;
   /**
@@ -19,8 +21,8 @@ export interface TdPopconfirmProps {
    */
   children?: TNode;
   /**
-   * 确认按钮
-   * @default '确定'
+   * 确认按钮，可自定义。值为 undefined 或 null 则不显示确认按钮
+   * @default ''
    */
   confirmBtn?: ButtonProps | TNode;
   /**
@@ -28,13 +30,40 @@ export interface TdPopconfirmProps {
    */
   content?: TNode;
   /**
+   * 是否在关闭浮层时销毁浮层
+   * @default true
+   */
+  destroyOnClose?: boolean;
+  /**
    * 确认框图标
    */
   icon?: TElement;
   /**
+   * 浮层出现位置
+   * @default top
+   */
+  placement?:
+    | 'top'
+    | 'left'
+    | 'right'
+    | 'bottom'
+    | 'top-left'
+    | 'top-right'
+    | 'bottom-left'
+    | 'bottom-right'
+    | 'left-top'
+    | 'left-bottom'
+    | 'right-top'
+    | 'right-bottom';
+  /**
    * 透传 Popup 组件属性
    */
   popupProps?: PopupProps;
+  /**
+   * 是否显示浮层箭头
+   * @default true
+   */
+  showArrow?: boolean;
   /**
    * 文字提示风格
    * @default default
@@ -57,18 +86,18 @@ export interface TdPopconfirmProps {
    */
   onCancel?: (options: { e: MouseEvent<HTMLDivElement | HTMLButtonElement> }) => void;
   /**
-   * 确认框显示或隐藏时触发
-   */
-  onChange?: (visible: boolean, context: ChangeContext) => void;
-  /**
    * 点击确认按钮时触发
    */
   onConfirm?: (options: { e: MouseEvent<HTMLDivElement | HTMLButtonElement> }) => void;
+  /**
+   * 确认框显示或隐藏时触发
+   */
+  onVisibleChange?: (visible: boolean, context?: PopconfirmVisibleChangeContext) => void;
 }
 
-export interface ChangeContext {
-  trigger: TriggerSource;
-  e: MouseEvent<HTMLDivElement>;
+export interface PopconfirmVisibleChangeContext {
+  trigger?: TriggerSource;
+  e?: MouseEvent<HTMLDivElement>;
 }
 
-export type TriggerSource = 'cancel' | 'confirm';
+export type TriggerSource = 'cancel' | 'confirm' | 'document' | 'trigger-element-click';
