5838298398b387a23cc5af851b0b9e9f99ad5486
uyarn <uyarnchen@gmail.com>
2021-11-25 17:24:50 +0800

    feat(select): collapse items

--- src/_type/components/select/index.ts
+++ src/_type/components/select/index.ts
@@ -1,13 +1,15 @@
+/* eslint-disable */
+
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
- * updated at 2021-06-02 17:34:44
+ * updated at 2021-11-25 20:07:16
  * */
 
-import { MouseEvent, FocusEvent } from 'react';
 import { PopupProps } from '../../../popup';
 import { TNode, TElement, SizeEnum } from '../../common';
+import { MouseEvent, KeyboardEvent, FocusEvent } from 'react';
 
-export interface TdSelectProps<SelectOption extends Options = Options> {
+export interface TdSelectProps<T extends SelectOption = SelectOption> {
   /**
    * 是否有边框
    * @default true
@@ -18,6 +20,10 @@ export interface TdSelectProps<SelectOption extends Options = Options> {
    * @default false
    */
   clearable?: boolean;
+  /**
+   * 多选情况下，用于设置折叠项内容，默认为 `+N`。如果需要悬浮就显示其他内容，可以使用 collapsedItems 自定义
+   */
+  collapsedItems?: TNode<{ value: T[]; collapsedSelectedItems: T[]; count: number }>;
   /**
    * 是否允许用户创建新条目，需配合 filterable 使用
    * @default false
@@ -30,13 +36,13 @@ export interface TdSelectProps<SelectOption extends Options = Options> {
   disabled?: boolean;
   /**
    * 当下拉列表为空时显示的内容
-   * @default '暂无数据'
+   * @default ''
    */
   empty?: TNode;
   /**
-   * 过滤方法，用于对现有数据进行搜索过滤，判断是否过滤某一项数据
+   * 自定义过滤方法，用于对现有数据进行搜索过滤，判断是否过滤某一项数据
    */
-  filter?: (filterWords: string, option: SelectOption) => boolean | Promise<boolean>;
+  filter?: (filterWords: string, option: T) => boolean | Promise<boolean>;
   /**
    * 是否可搜索
    * @default false
@@ -47,13 +53,13 @@ export interface TdSelectProps<SelectOption extends Options = Options> {
    */
   keys?: SelectKeysType;
   /**
-   * 是否正在加载数据
+   * 是否为加载状态
    * @default false
    */
   loading?: boolean;
   /**
    * 远程加载时显示的文字，支持自定义。如加上超链接
-   * @default '加载中'
+   * @default ''
    */
   loadingText?: TNode;
   /**
@@ -61,6 +67,11 @@ export interface TdSelectProps<SelectOption extends Options = Options> {
    * @default 0
    */
   max?: number;
+  /**
+   * 最小折叠数量，用于多选情况下折叠选中项，超出该数值的选中项折叠。值为 0 则表示不折叠
+   * @default 0
+   */
+  minCollapsedNum?: number;
   /**
    * 是否允许多选
    * @default false
@@ -70,7 +81,7 @@ export interface TdSelectProps<SelectOption extends Options = Options> {
    * 数据化配置选项内容
    * @default []
    */
-  options?: Array<SelectOption>;
+  options?: Array<T>;
   /**
    * 占位符
    * @default ''
@@ -103,6 +114,10 @@ export interface TdSelectProps<SelectOption extends Options = Options> {
    */
   defaultValue?: SelectValue;
   /**
+   * 自定义选中项呈现方式
+   */
+  valueDisplay?: TNode<{ value: T[]; onClose: () => void }>;
+  /**
    * 用于控制选中值的类型。假设数据选项为：[{ label: '姓名', value: 'name' }]，value 表示值仅返回数据选项中的 value， object 表示值返回全部数据。
    * @default value
    */
@@ -124,27 +139,43 @@ export interface TdSelectProps<SelectOption extends Options = Options> {
    */
   onCreate?: (value: string | number) => void;
   /**
+   * 回车键按下时触发。`inputValue` 表示输入框的值，`value` 表示选中值
+   */
+  onEnter?: (context: { inputValue: string; e: KeyboardEvent<HTMLDivElement>; value: SelectValue }) => void;
+  /**
    * 输入框获得焦点时触发
    */
   onFocus?: (context: { value: SelectValue; e: FocusEvent<HTMLDivElement> }) => void;
   /**
    * 多选模式下，选中数据被移除时触发
    */
-  onRemove?: (options: SelectRemoveContext<SelectOption>) => void;
+  onRemove?: (options: SelectRemoveContext<T>) => void;
   /**
    * 输入值变化时，触发搜索事件。主要用于远程搜索新数据
    */
   onSearch?: (filterWords: string) => void;
+  /**
+   * 下拉框隐藏/显示时触发
+   */
+  onVisibleChange?: (visible: boolean) => void;
 }
 
 export interface TdOptionProps {
+  /**
+   * 用于定义复杂的选项内容，同 content
+   */
+  children?: TNode;
+  /**
+   * 用于定义复杂的选项内容
+   */
+  content?: TNode;
   /**
    * 是否禁用该选项
    * @default false
    */
   disabled?: boolean;
   /**
-   * 选项描述（若不设置则默认与 value 相同）
+   * 选项名称
    * @default ''
    */
   label?: string;
@@ -155,6 +186,11 @@ export interface TdOptionProps {
 }
 
 export interface TdOptionGroupProps {
+  /**
+   * 是否显示分隔线
+   * @default true
+   */
+  divider?: boolean;
   /**
    * 分组别名
    * @default ''
@@ -167,11 +203,7 @@ export interface SelectKeysType {
   label?: string;
 }
 
-export type SelectValue<SelectOption extends Options = Options> =
-  | string
-  | number
-  | SelectOption
-  | Array<SelectValue<SelectOption>>;
+export type SelectValue<T extends SelectOption = SelectOption> = string | number | T | Array<SelectValue<T>>;
 
 export interface SelectRemoveContext<T> {
   value: string | number;
@@ -179,4 +211,9 @@ export interface SelectRemoveContext<T> {
   e: MouseEvent<HTMLDivElement>;
 }
 
-export type Options = { label?: string; value?: string | number; disabled?: boolean } & Record<string, any>;
+export type SelectOption = TdOptionProps | SelectOptionGroup;
+
+export interface SelectOptionGroup extends TdOptionGroupProps {
+  group: string;
+  children: Array<TdOptionProps>;
+}
